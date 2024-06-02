9aba75a34c56a6f05a123ee707db0a5d24293ed8
tusqasi <2002.tushar.kuntawar@gmail.com>
2024-06-02 21:58:01 +0530

    Variable correction

--- main.exs
+++ main.exs
@@ -2,15 +2,15 @@ if length(System.argv()) != 2 do
   exit("Incorrect arguments passed")
 end
 
-[round, last_choice] = System.argv()
-if round > 200  do
+[round_n, last_choice] = System.argv()
+if String.to_integer(round_n) > 200  do
   exit("Too many rounds")
 end
 
 folder_name = "tusqasi_output"
 File.mkdir_p!(folder_name)
 
-File.touch(Path.join(folder_name, round))
-File.write(Path.join(folder_name, round), last_choice)
+File.touch(Path.join(folder_name, round_n))
+File.write(Path.join(folder_name, round_n), last_choice)
 
 IO.puts(last_choice)
